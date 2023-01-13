36cbe00fbe28ba605732a9d98786ca997a9419fd
Clément Parssegny <clement.parssegny@ssi.gouv.fr>
2023-01-13 09:31:11 +0100

    Updated license according to discussions

--- cve_debian.py
+++ cve_debian.py
@@ -2,13 +2,13 @@
 Software Name : decret (DEbian Cve REproducer Tool)
 Version : 0.1
 SPDX-FileCopyrightText : Copyright (c) 2022
-SPDX-License-Identifier : MIT
+SPDX-License-Identifier : BSD-3-Clause
 
-This software is distributed under the MIT License,
-the text of which is available at https://mit-license.org/
+This software is distributed under the BSD 3-Clause "New" or "Revised" License,
+the text of which is available at https://opensource.org/licenses/BSD-3-Clause
 or see the "license.txt" file for more not details.
 
-Author : Clément PARSSEGNY, Olivier LEVILLAIN
+Author : Clément PARSSEGNY, Olivier LEVILLAIN, Maxime BELAIR
 Software description : A tool to reproduce vulnerability affecting Debian
 It gathers details from the Debian metadata and exploits from exploit-db.com
 in order to build and run a vulnerable Docker container to test and
